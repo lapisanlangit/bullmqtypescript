b2a0f66df423f9f1b5707aafc44afae61b786f6b
Umar Jati <lapisanlangit@gmail.com>
2025-07-06 13:12:37 +0700

    add nodemon

--- src/index.ts
+++
@@ -1,5 +0,0 @@
-// Example: Import and run producer
-import { addJobs, addJobs2 } from "./producer";
-
-addJobs().catch(console.error);
-addJobs2().catch(console.error);
--- src/producer.ts
+++ src/producer.ts
@@ -13,18 +13,10 @@ const myQueue = new Queue("my-queue", { connection });
 
 // Add jobs to the queue
 export async function addJobs() {
-  await myQueue.add("myJobName", { foo: "bar" });
+  await myQueue.add("myJobName", { foo: "bar" }, { delay: 3000 });
   console.log("Jobs added to queue");
 }
 
-const myQueue2 = new Queue("my-queue2", { connection });
-
-// Add jobs to the queue
-export async function addJobs2() {
-  await myQueue2.add("myJobName2", { qux: "bazzzz" });
-  console.log("Jobs22 added to queue");
-}
-
 // Run the producer
-// addJobs().catch(console.error);
+addJobs().catch(console.error);
 // addJobs2().catch(console.error);
